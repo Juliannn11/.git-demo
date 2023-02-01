697d79731a03a730387151585a34eb8cd624ebbe
1211104262 <1211104262@student.mmu.edu.my>
2023-02-01 21:44:56 +0800

    added new version of Part1code.cpp

--- Part1code.cpp
+++ Part1code.cpp
@@ -4,7 +4,7 @@
 // Lab: TT5L
 // Names: Julian Vijay | Khan Anas Adnan | Ahmad Muhaimin Luqman Bin Husini
 // IDs: 1211104262 | 1211306594 | 1221303795
-// Emails: 1211104262@student.mmu.edy.my| 1211306594@student.mmu.edy.my | 1221303795@student.mmu.edy.my
+// Emails: 1211104262@student.mmu.edu.my| 1211306594@student.mmu.edu.my | 1221303795@student.mmu.edu.my
 // Phones: 0147933686 | 01131556827 | 0162251701
 // *********************************************************
 
