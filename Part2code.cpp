8796ca846addd88e0325ad3a1e0ee1ded9a6e4e3
Julian Vijay <1211104262@student.mmu.edu.my>
2023-02-19 20:40:56 +0800

    add part2code.cpp

---
+++ Part2code.cpp
@@ -0,0 +1,546 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib> // for system()
+#include <ctime>   // for time() in srand( time(NULL) );
+#include <iomanip> // for setw()
+#include <cmath>
+#include <conio.h>
+
+using namespace std;
+
+class Alien
+{
+    int LIFE = 100;
+    int ATTACK = 0;
+
+public:
+    const char Acharacter = 'A';
+    void printA()
+    {
+        cout << " --> Alien : "
+             << "life :- " << LIFE << ", Attack :- " << ATTACK << endl;
+    }
+    void health_plus()
+    {
+        if (LIFE + 20 > 100)
+        {
+            LIFE = 100;
+            cout << "Alien finds a health pack. But, its at Maximium cap so no further increase in life.";
+        }
+        else
+        {
+            LIFE += 20;
+            cout << "Alien finds a health pack." << endl;
+            cout << "Alien's life increase by 20" << endl;
+        }
+    }
+    void attack_plus()
+    {
+        ATTACK += 20;
+    }
+
+};
+
+
+class board
+{
+private:
+    vector<vector<char>> map_;
+    int n_, m_, z_;
+
+public:
+    void setboard(int n, int m, int z)
+    {
+        n_ = n;
+        m_ = m;
+        z_ = z;
+    }
+
+    void obj_in_board(int n, int m);
+    void display() const;
+    void set_object(int x, int y, char c);
+    char get_object(int x, int y) const;
+    bool isInsideMap(int n, int m);
+    int getn() const;
+    int getm() const;
+    int getz() const;
+};
+class Move
+{
+private:
+    string com_;
+    int x_, y_;
+    int zombiex_[9];
+    int zombiey_[9];
+    Alien A_;
+    char Z_[9];
+    char Zcharacter[9];
+
+public:
+    Move();
+    void setcom(string com)
+    {
+        com_ = com;
+    }
+
+    void alien_move(board &Board);
+    void A(board &Board);
+    char getA() const;
+    int getx() const;
+    int gety() const;
+    void Z(board &Board);
+    void zombie_move(board &Board);
+    void help(board &Board);
+    void rock(board &Board);
+};
+
+void Move::rock(board &Board)
+{   srand(time(NULL));
+    int x = x_;
+    int y = y_;
+    char r = Board.get_object(x, y);
+    int num = rand() % 7;
+    if (num == 0)
+    {
+        cout << "Alien discover an up arrow beneath the rock" << endl;
+    }
+    else if (num == 1)
+    {
+        cout << "Alien discover an left arrow beneath the rock" << endl;
+    }
+    else if (num == 2)
+    {
+        cout << "Alien discover an right arrow beneath the rock" << endl;
+    }
+    else if (num == 3)
+    {
+        cout << "Alien discover an down arrow beneath the rock" << endl;
+    }
+    else if (num == 4)
+    {
+        cout << "Alien discover a pod beneath the rock" << endl;
+    }
+    else if (num == 5)
+    {
+        cout << "Alien discover an health pack beneath the rock" << endl;
+    }
+    else
+    {
+        cout << "There is nothing beneath the rock" << endl;
+    }
+
+    cout << endl;
+    Board.set_object(x, y, r);
+}
+
+Move::Move()
+{
+}
+void Move::zombie_move(board &Board)
+{
+    srand(time(NULL));
+    for (int i = 0; i < Board.getz(); i++)
+    {
+
+        int zx = zombiex_[i];
+        int zy = zombiey_[i];
+        int num = rand() % 4;
+
+        if (num == 0)
+        {
+            zy += 1;
+            cout << "Zombie move up" << endl;
+        }
+        else if (num == 1)
+        {
+            zx -= 1;
+            cout << "Zombie move left" << endl;
+        }
+        else if (num == 2)
+        {
+            zx += 1;
+            cout << "Zombie move right" << endl;
+        }
+        else
+        {
+            zy -= 1;
+            cout << "Zombie move down" << endl;
+        }
+
+        cout << "Press enter to continue... " << endl;
+        getch();
+
+        if (Board.isInsideMap(zx, zy))
+        {
+            char obstacle = Board.get_object(zx, zy);
+            if (obstacle == 'A')
+            {
+                cout << "Zombie stop moving" << endl;
+            }
+            else if (obstacle >= '1' && obstacle <= '9')
+            {
+                cout << "Zombie stop moving" << endl;
+            }
+            else
+            {
+                Board.set_object(zombiex_[i], zombiey_[i], ' '); // Remove old
+                zombiex_[i] = zx;
+                zombiey_[i] = zy;
+                Board.set_object(zombiex_[i], zombiey_[i], Z_[i]); // New
+            }
+        }
+        else
+        {
+            cout << "zombie reach the border" << endl;
+        }
+        Board.display();
+    }
+}
+void Move::help(board &Board)
+{
+    cout << "Commands" << endl;
+    cout << "1. up     - Move up." << endl;
+    cout << "2. down   - Move down." << endl;
+    cout << "3. left   - Move left." << endl;
+    cout << "4. right  - Move right." << endl;
+    cout << "5. arrow  - Change the direction of an arrow." << endl;
+    cout << "6. help   - Display these user commands." << endl;
+    cout << "7. save   - Save the game." << endl;
+    cout << "8. load   - Load a game." << endl;
+    cout << "9. quit   - Quit the game." << endl;
+}
+void Move::Z(board &Board)
+{
+    char Zcharacter[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    for (int i = 0; i < Board.getz(); i++)
+    {
+        int x = rand() % Board.getn() + 1;
+        int y = rand() % Board.getm() + 1;
+        zombiex_[i] = x;
+        zombiey_[i] = y;
+        Z_[i] = Zcharacter[i];
+        Board.set_object(zombiex_[i], zombiey_[i], Z_[i]);
+    }
+}
+void Move::A(board &Board)
+{
+    x_ = Board.getn() / 2 + 1;
+    y_ = Board.getm() / 2 + 1;
+    A_.Acharacter;
+    Board.set_object(x_, y_, A_.Acharacter);
+}
+
+void Move::alien_move(board &Board)
+{
+    Move Move;
+    int x = 0, y = 0;
+    int attack_ = 0;
+    int dimx = Board.getn();
+    int dimy = Board.getm();
+
+    if (com_ == "up")
+        y = 1;
+    else if (com_ == "down")
+        y = -1;
+    else if (com_ == "left")
+        x = -1;
+    else if (com_ == "right")
+        x = 1;
+
+
+
+    do
+    {
+        int new_x = x_ + x;// Update position of the alien
+        int new_y = y_ + y;
+        if (Board.isInsideMap(new_x, new_y))
+        {
+            Board.set_object(x_, y_, '.');
+            x_ = new_x;
+            y_ = new_y;
+
+            if (Board.get_object(x_, y_) == '^')
+            {
+                x = 0;
+                y = 1;
+                A_.attack_plus();
+            }
+            else if (Board.get_object(x_, y_) == '>')
+            {
+                x = 1;
+                y = 0;
+                A_.attack_plus();
+            }
+            else if (Board.get_object(x_, y_) == 'v')
+            {
+                x = 0;
+                y = -1;
+                A_.attack_plus();
+            }
+            else if (Board.get_object(x_, y_) == '<')
+            {
+                x = -1;
+                y = 0;
+                A_.attack_plus();
+            }
+            else if (Board.get_object(x_, y_) == 'h')
+            {
+
+                A_.health_plus();
+            }
+            else if (Board.get_object(x_, y_) == 'p')
+            {
+            }
+            else if (Board.get_object(x_, y_) == 'r')
+            {
+                cout << "Alien stumbles upon a rock.";
+                rock(Board);
+            }
+        }
+        cout << "Press any key to continue..." << endl;
+        getch();
+        Board.set_object(x_, y_, A_.Acharacter);
+        Board.display();
+    } while (Board.isInsideMap(x_ + x, y_ + y));
+    // Update position of the alien on the board
+}
+
+int Move::getx() const
+{
+    return x_;
+}
+
+int Move::gety() const
+{
+    return y_;
+}
+
+char Move::getA() const
+{
+    return A_.Acharacter;
+}
+
+void board::set_object(int x, int y, char c)
+{
+    map_[(m_ - y)][x - 1] = c;
+}
+int board::getz() const
+{
+    return z_;
+}
+int board::getn() const
+{
+    return n_;
+}
+
+int board::getm() const
+{
+    return m_;
+}
+
+void board::obj_in_board(int n, int m)
+{
+    srand(time(NULL));
+    n_ = n;
+    m_ = m;
+    char objects[] = {' ', '<', 'p', '^', 'r', 'v', 'h', '>'};
+    int noofObjects = 8;
+    map_.resize(m_); // empty row
+    for (int i = 0; i < m_; ++i)
+    {
+        map_[i].resize(n_);
+    }
+    for (int i = 0; i < m_; ++i)
+    {
+        for (int j = 0; j < n_; ++j)
+        {
+
+            int objNo = rand() % noofObjects;
+            map_[i][j] = objects[objNo];
+        }
+    }
+}
+
+void board::display() const
+{
+    board Board;
+    Move Move;
+    int life;
+    int attack;
+    int range;
+    cout << ".: Alien vs Zombie :." << endl;
+    for (int i = 0; i < m_; ++i) // row
+    {
+        cout << " ";
+        for (int j = 0; j < n_; ++j) // column
+        {
+            cout << "+-";
+        }
+        cout << "+-" << endl;
+        cout << (m_ - i); // row number
+        for (int j = 0; j < n_; ++j)
+        {
+            cout << "|" << map_[i][j];
+        }
+        cout << "| " << endl;
+    }
+    cout << " ";
+    for (int j = 0; j < n_; ++j) // lower border
+    {
+        cout << "+-";
+    }
+    cout << "+" << endl;
+    cout << " ";
+    for (int j = 0; j < n_; ++j) // column number
+    {
+        int digit = (j + 1) / 10;
+        cout << " ";
+        if (digit == 0)
+            cout << " ";
+        else
+            cout << digit;
+    }
+    cout << endl;
+    cout << " ";
+    for (int j = 0; j < n_; ++j)
+    {
+        cout << " " << (j + 1) % 10;
+    }
+    cout << endl
+         << endl;
+
+    const int MIN_LIFE_ZOMBIE = 100;
+    const int MAX_LIFE_ZOMBIE = 300;
+    const int MIN_LIFE_ALIEN = 100;
+    const int MAX_LIFE_ALIEN = 100;
+    const int MIN_ATTACK = 1;
+    const int MAX_ATTACK = 15;
+    const int MIN_RANGE = 1;
+    const int MAX_RANGE = 3;
+
+    cout << "Alien : Life 100"
+         << ", Attack :0" << endl;
+
+    for (int zombie_amount = 1; zombie_amount <= z_; ++zombie_amount)
+    {
+        srand(zombie_amount);
+        life = rand() % (MAX_LIFE_ZOMBIE - MIN_LIFE_ZOMBIE + 1) + MIN_LIFE_ZOMBIE;
+        attack = rand() % (MAX_ATTACK - MIN_ATTACK + 1) + MIN_ATTACK;
+        range = rand() % (MAX_RANGE - MIN_RANGE + 1) + MIN_RANGE;
+
+        cout << "Zombie " << zombie_amount << ": Life " << life << ", Attack " << attack << ", Range " << range << endl;
+    }
+}
+
+char board::get_object(int x, int y) const
+{
+    return map_[m_ - y][x - 1];
+}
+
+bool board ::isInsideMap(int x, int y) // within map boarder
+{
+    return x >= 1 && x <= n_ && y >= 1 && y <= m_;
+}
+
+
+void test1()
+{
+
+    string com;
+    Move Move;
+    board Board;
+    int n = 5;
+    int m = 9;
+    int z = 3;
+    string setting;
+    cout << "Defaut Game Settings " << endl;
+    cout << "-----------------------" << endl;
+    cout << "Board Rows :" << n << endl;
+    cout << "Board Columns :" << m << endl;
+    cout << "Zombie Count :" << z << endl;
+    cout << "Do you wish to change the game setting (y/n)? => ";
+    cin >> setting;
+
+    if (setting == "y" || setting == "Y")
+    {
+        int settingtest = 0;
+        while (settingtest < 1)
+        {
+            settingtest = settingtest + 1;
+            cout << "Board Settings" << endl;
+            cout << "-----------------" << endl;
+            bool settingreset;
+            do
+            {
+                cout << "Enter rows =>";
+                cin >> n;
+                cout << "Enter columns =>";
+                cin >> m;
+                if (n % 2 == 0 || m % 2 == 0)
+                {
+                    cout << "Please enter odd number" << endl;
+                    settingreset = true;
+                }
+                else
+                {
+                    settingreset = false;
+                }
+            } while (settingreset);
+            cout << " " << endl;
+            cout << "Zombie Settings" << endl;
+            cout << "----------------- " << endl;
+            do
+            {
+                cout << "Enter number of zombies => ";
+                cin >> z;
+                if (z > 9)
+                {
+                    cout << "The maximum number of zombie is 9." << endl;
+                }
+            } while (z > 9);
+            cout << "Settings Updated." << endl;
+            cout << " " << endl;
+            cout << "Press any key to continue..." << endl;
+            getch();
+        }
+    }
+    Board.setboard(n, m, z);
+    Board.obj_in_board(n, m);
+    Move.Z(Board);
+    Move.A(Board);
+    Board.display();
+
+    string command;
+    while (command != "quit")
+    {
+        cout << "command>";
+        cin >> command;
+        if (command == "up" || command == "down" || command == "left" || command == "right")
+        {
+            Move.setcom(command);
+            Move.alien_move(Board);
+            Move.zombie_move(Board);
+        }
+        else if (command == "help")
+        {
+            Move.help(Board);
+        }
+
+        else if (command == "quit")
+        {
+            cout << "Exiting program..." << endl;
+        }
+        else
+        {
+            cout << "Invalid command. Please try again." << endl;
+        }
+    }
+}
+
+int main()
+{
+    srand(time(NULL));
+
+    test1();
+}
